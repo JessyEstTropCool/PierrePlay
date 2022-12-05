4c5f58189a077caccdb5fe36da59b1369e7e6471
Jessy Adam <80044343+JessyEstTropCool@users.noreply.github.com>
2022-12-05 17:31:52 +0100

    Tests p1

---
+++ src/test/java/be/helb/PierrePlay/services/GameServiceTest.java
@@ -0,0 +1,35 @@
+package be.helb.PierrePlay.services;
+
+import be.helb.PierrePlay.daos.GameDao;
+import be.helb.PierrePlay.models.Game;
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameServiceTest
+{
+    private GameDao gameDao;
+
+    private GameService gameService;
+
+    @Test
+    public void testGetAllGames()
+    {
+        List<Game> games = List.of(new Game("Supah Maro"),new Game("Sonk da hedg"),new Game("Kirbo goes"));
+
+        gameDao = EasyMock.mock(GameDao.class);
+        EasyMock.expect(gameDao.findAll()).andReturn(games);
+        EasyMock.expect(gameDao.findAll()).andReturn(games);
+        EasyMock.expect(gameDao.findAll()).andReturn(games);
+        EasyMock.expect(gameDao.findAll()).andReturn(games);
+        gameService = new GameService(gameDao);
+        EasyMock.replay(gameDao);
+        int result = gameService.getAll().size();
+        EasyMock.verify(gameDao);
+        assertEquals(3, result);
+        //gameDao.findAll();
+    }
+}
